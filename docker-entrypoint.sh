d8a5b47c4004be7cd7dd32765c892ba7ac5f4991
Harald Leithner <leithner@itronic.at>
2022-06-11 17:38:48 +0200

    Add signature signing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,9 @@ set -o pipefail
 set -o errexit
 
 basedir=`pwd`
+TUF="/tuf/bin/tuf --insecure-plaintext"
+GIT="/usr/bin/git"
+GH="/usr/bin/gh"
 
 if [[ -n ${DEBUG:-} ]]; then
   echo "=> DEBUG is enabled"
@@ -15,62 +18,116 @@ if [[ -n ${DEBUG:-} ]]; then
 fi
 
 echo "=> Setup Github CLI"
-#/usr/bin/gh
 /usr/bin/gh auth login --with-token <<< "${ACCESS_TOKEN}"
 
 echo "=> Configure git"
-/usr/bin/git config --global user.name "${GIT_USER_NAME}"
-/usr/bin/git config --global user.email "${GIT_USER_EMAIL}"
-/usr/bin/git config --global init.defaultBranch main
-/usr/bin/git config --global pull.rebase false;
-/usr/bin/git config --global credential.https://github.com.username git
+$GIT config --global user.name "${GIT_USER_NAME}"
+$GIT config --global user.email "${GIT_USER_EMAIL}"
+$GIT config --global init.defaultBranch "${GIT_BASE_BRANCH_NAME}"
+$GIT config --global pull.rebase false;
+$GIT config --global credential.https://github.com.username git
 
-if [[ -d .git ]]; then
-  /usr/bin/git checkout ${GIT_BRANCH_NAME}
-  /usr/bin/git fetch origin
-  /usr/bin/git branch --set-upstream-to=origin/${GIT_BRANCH_NAME} ${GIT_BRANCH_NAME}
-  /usr/bin/git pull
-  /usr/bin/git fetch origin
-  /usr/bin/git rebase origin/main
-else
-  /usr/bin/git init
-  /usr/bin/git remote add origin "${GIT_URL}"
-  /usr/bin/git fetch origin
-  /usr/bin/git checkout "${GIT_BRANCH_NAME}"
-  /usr/bin/git rebase origin/main
+# If we already are in a directory with a git repository we assume it's the
+# update repository and fetch the latest commit. If we are not in a git
+# repository we create a new one and pull the upstream repository
+if [[ ! -d .git ]]; then
+  echo "=> Initially checkout repository"
+  $GIT init
+  $GIT remote add origin "${GIT_URL}"
+  $GIT fetch origin
+  $GIT checkout "${GIT_BASE_BRANCH_NAME}"
+
 fi
 
-if [[ $1 == "update-timestamp" ]]; then
-  echo "=> TUF Updation timestamp"
-  /tuf/bin/tuf timestamp
-  /tuf/bin/tuf commit
-  /usr/bin/git commit -am "Update timestamp"
-  /usr/bin/git push
-elif [[ $1 == "release" ]]; then
-  /tuf/bin/tuf snapshot
-  /tuf/bin/tuf timestamp
-  /tuf/bin/tuf commit
-  /usr/bin/git add .
-  /usr/bin/git commit -m "Release: ${GIT_BRANCH_NAME}"
-  /usr/bin/git push
-  PR_URL=$(/usr/bin/gh pr create --base main --title "Release: ${GIT_BRANCH_NAME}")
-  /usr/bin/gh pr merge "${PR_URL}"
-elif [[ $1 == "prepare-release" ]]; then
-  sed "s/\$VERSION/${UPDATE_VERSION}/g" $basedir/templates/update-info-4.json | tee /tmp/update-info.json
-  cat <<< $(jq '.["description"] = "'"${UPDATE_DESCRIPTION}"'"' /tmp/update-info.json) > /tmp/update-info.json
-  cat <<< $(jq '.["infourl"]["url"] = "'"${UPDATE_INFO_URL}"'"' /tmp/update-info.json) > /tmp/update-info.json
-  cat <<< $(jq '.["infourl"]["title"] = "'"${UPDATE_INFO_TITLE}"'"' /tmp/update-info.json) > /tmp/update-info.json
-  cat <<< $(jq '.["name"] = "'"${UPDATE_NAME}"'"' /tmp/update-info.json) > /tmp/update-info.json
-  cat <<< $(jq '.["version"] = "'"${UPDATE_VERSION}"'"' /tmp/update-info.json) > /tmp/update-info.json
-  /tuf/bin/tuf add --custom="$(jq -c '.' /tmp/update-info.json)"
-  /usr/bin/git add .
-  /usr/bin/git commit -m "Prepare ${UPDATE_VERSION}"
-  /usr/bin/git push
-elif [[ $1 == "sign-release" ]]; then
-  /tuf/bin/tuf sign targets.json
-  git add .
-  git commit -m "Sign Release ${GIT_BRANCH_NAME}"
-  git push
+echo "=> Update repository version"
+$GIT fetch origin
+
+if [ "`$GIT show-branch ${GIT_TARGET_BRANCH_NAME} > /dev/null || echo $?`" == "" ]; then
+  $GIT checkout ${GIT_TARGET_BRANCH_NAME}
 else
-  /tuf/bin/tuf "$1"
+  $GIT checkout -b ${GIT_TARGET_BRANCH_NAME}
 fi
+
+if [ "`$GIT show-branch origin/${GIT_TARGET_BRANCH_NAME} > /dev/null || echo $?`" == "" ]; then
+  $GIT branch --set-upstream-to=origin/${GIT_TARGET_BRANCH_NAME} ${GIT_TARGET_BRANCH_NAME}
+  $GIT pull
+fi
+
+$GIT fetch origin
+$GIT rebase origin/${GIT_BASE_BRANCH_NAME}
+
+case "$1" in
+  "bash")
+      /bin/bash
+      ;;
+  "update-timestamp")
+      echo "=> TUF Updation timestamp"
+      $TUF timestamp
+      $TUF commit
+      $GIT commit -am "Update timestamp"
+      $GIT push -u origin ${GIT_TARGET_BRANCH_NAME}
+      ;;
+  "prepare-release")
+      sed "s/\$VERSION/${UPDATE_VERSION}/g" $basedir/templates/update-info-4.json | tee /tmp/update-info.json
+      cat <<< $(jq '.["description"] = "'"${UPDATE_DESCRIPTION}"'"' /tmp/update-info.json) > /tmp/update-info.json
+      cat <<< $(jq '.["infourl"]["url"] = "'"${UPDATE_INFO_URL}"'"' /tmp/update-info.json) > /tmp/update-info.json
+      cat <<< $(jq '.["infourl"]["title"] = "'"${UPDATE_INFO_TITLE}"'"' /tmp/update-info.json) > /tmp/update-info.json
+      cat <<< $(jq '.["name"] = "'"${UPDATE_NAME}"'"' /tmp/update-info.json) > /tmp/update-info.json
+      cat <<< $(jq '.["version"] = "'"${UPDATE_VERSION}"'"' /tmp/update-info.json) > /tmp/update-info.json
+      $TUF add --custom="$(jq -c '.' /tmp/update-info.json)"
+      $GIT add .
+      $GIT commit -m "Prepare ${UPDATE_VERSION}"
+      $GIT push -u origin ${GIT_TARGET_BRANCH_NAME}
+      ;;
+  "sign-release")
+      $TUF sign targets.json
+      git add .
+      git commit -m "Sign Release ${GIT_TARGET_BRANCH_NAME}"
+      $GIT push -u origin ${GIT_TARGET_BRANCH_NAME}
+      ;;
+  "release")
+      $TUF snapshot
+      $TUF timestamp
+      $TUF commit
+      $GIT add .
+      $GIT commit -m "Release: ${GIT_TARGET_BRANCH_NAME}"
+      $GIT push -u origin ${GIT_TARGET_BRANCH_NAME}
+      PR_URL=$($GH pr create --base ${GIT_BASE_BRANCH_NAME} --title "Release: ${GIT_TARGET_BRANCH_NAME}")
+      $GH pr merge "${PR_URL}"
+      ;;
+  "create-signature")
+      tmpfile=`mktemp`
+      $TUF gen-key --expires=1825 ${SIGNATURE_ROLE} | tee $tmpfile
+      keyid=`cat $tmpfile | awk '{print $6}'`
+
+      jq ". + {\"${keyid}\":\"${SIGNATURE_ROLE_NAME}\"}" < metadata/keys.json > $tmpfile
+      mv $tmpfile metadata/keys.json
+
+      $TUF payload ${SIGNATURE_ROLE}.json > staged/${SIGNATURE_ROLE}.json.payload
+      $GIT add .
+      $GIT commit -m "Prepare key for Role ${SIGNATURE_ROLE} by ${SIGNATURE_ROLE_NAME}"
+      $GIT push -u origin ${GIT_TARGET_BRANCH_NAME}
+      ;;
+  "sign-signature")
+      $TUF sign-payload --role=${SIGNATURE_ROLE} staged/${SIGNATURE_ROLE}.json.payload > staged/${SIGNATURE_ROLE}.json.sigs.$RANDOM
+      $GIT add .
+      $GIT commit -m "Signed key for Role ${SIGNATURE_ROLE}"
+      $GIT push -u origin ${GIT_TARGET_BRANCH_NAME}
+      ;;
+  "commit-signature")
+      jq -s add staged/${SIGNATURE_ROLE}.json.sigs.* > staged/${SIGNATURE_ROLE}.json.sigs
+      $TUF add-signatures --signatures staged/${SIGNATURE_ROLE}.json.sigs ${SIGNATURE_ROLE}.json
+      rm staged/${SIGNATURE_ROLE}.json.*
+      $TUF snapshot
+      $TUF timestamp
+      $TUF commit
+      $GIT add .
+      $GIT commit -m "Add key for Role ${SIGNATURE_ROLE}"
+      $GIT push --force -u origin ${GIT_TARGET_BRANCH_NAME}
+      PR_URL=$($GH pr create --base ${GIT_BASE_BRANCH_NAME} --title "Add key for role ${SIGNATURE_ROLE}" --body "Add key for role ${SIGNATURE_ROLE}")
+      $GH pr merge --merge "${PR_URL}"
+      ;;
+  *)
+      $TUF "$1"
+      ;;
+esac
--- sign.sh
+++ sign.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 DOCKER_IMAGE="joomlaprojects/docker-images:updater"
+DOCKER_IMAGE="joomla-tuf:latest"
+
+GIT_BASE_BRANCH_NAME=main
+GIT_TARGET_BRANCH_NAME=next
+
+TUF_TARGETS_PASSPHRASE=""
+TUF_TIMESTAMP_PASSPHRASE=""
+TUF_SNAPSHOT_PASSPHRASE=""
+TUF_ROOT_PASSPHRASE=""
 
 if [ -f .env ];
 then
@@ -37,8 +46,8 @@ then
 fi
 
 echo "=> Asking for needed User inputs"
-localread "Branch to use for signage:" "" "GIT_BRANCH_NAME"
-if [ -z "${GIT_BRANCH_NAME}" ]; then echo "Aboarting no branch name given."; exit 1; fi
+localread "Branch to use for signage:" "" "GIT_TARGET_BRANCH_NAME"
+if [ -z "${GIT_TARGET_BRANCH_NAME}" ]; then echo "Aboarting no branch name given."; exit 1; fi
 
 localread "Github Personnel Access Token:" "" ACCESS_TOKEN s
 if [ -z "${ACCESS_TOKEN}" ]; then echo "Aboarting no Personnel Access Token given."; exit 1; fi
@@ -48,20 +57,34 @@ echo "Supported actions:"
 echo " * prepare-release"
 echo " * release"
 echo " * sign-release"
+echo " * create-signature"
+echo " * sign-signature"
+echo " * commit-signature"
 echo " * update-timestamp"
 echo " * bash"
 echo ""
 localread "Action to be passed to TUF:" "" TUF_PARAMS
 
-echo "=> Run signing process"
+# Prepare standard environment parameters for the docker iamge
+DOCKER_ENV_FILE=`mktemp`
+echo $DOCKER_ENV_FILE;
+echo "ACCESS_TOKEN=${ACCESS_TOKEN}" >> $DOCKER_ENV_FILE
+echo "GIT_BASE_BRANCH_NAME=${GIT_BASE_BRANCH_NAME}" >> $DOCKER_ENV_FILE
+echo "GIT_TARGET_BRANCH_NAME=${GIT_TARGET_BRANCH_NAME}" >> $DOCKER_ENV_FILE
+echo "GIT_USER_NAME=${GIT_USER_NAME}" >> $DOCKER_ENV_FILE
+echo "GIT_USER_EMAIL=${GIT_USER_EMAIL}" >> $DOCKER_ENV_FILE
+echo "TUF_TARGETS_PASSPHRASE=${TUF_TARGETS_PASSPHRASE}" >> $DOCKER_ENV_FILE
+echo "TUF_TIMESTAMP_PASSPHRASE=${TUF_TIMESTAMP_PASSPHRASE}" >> $DOCKER_ENV_FILE
+echo "TUF_SNAPSHOT_PASSPHRASE=${TUF_SNAPSHOT_PASSPHRASE}" >> $DOCKER_ENV_FILE
+echo "TUF_ROOT_PASSPHRASE=${TUF_ROOT_PASSPHRASE}" >> $DOCKER_ENV_FILE
+
+echo "=> Run TUF process"
 
 if [[ $TUF_PARAMS = "bash" ]]; then
-    docker run -ti --entrypoint /bin/bash \
-        -e ACCESS_TOKEN="${ACCESS_TOKEN}"\
-        -e GIT_BRANCH_NAME="${GIT_BRANCH_NAME}" \
-        -e GIT_USER_NAME="${GIT_USER_NAME}" \
-        -e GIT_USER_EMAIL="${GIT_USER_EMAIL}" \
-        -v "$(pwd)/updates:/go" ${DOCKER_IMAGE}
+    docker run -ti \
+        --env-file $DOCKER_ENV_FILE \
+        -v "$(pwd)/updates:/go" ${DOCKER_IMAGE} \
+        "${TUF_PARAMS}"
 elif [[ $TUF_PARAMS = "prepare-release" ]]; then
     echo "=> Add update files ans sign them"
     localread "Please enter the Update Version:" "" UPDATE_VERSION
@@ -70,25 +93,32 @@ elif [[ $TUF_PARAMS = "prepare-release" ]]; then
     localread "Please enter the Update Info URL:" "https://www.joomla.org/announcements/release-news/" UPDATE_INFO_URL
     localread "Please enter the Update Info Titel:" "${UPDATE_NAME} Release" UPDATE_INFO_TITLE
     docker run \
-        -e ACCESS_TOKEN="${ACCESS_TOKEN}"\
-        -e GIT_BRANCH_NAME="${GIT_BRANCH_NAME}"\
-        -e GIT_USER_NAME="${GIT_USER_NAME}"\
-        -e GIT_USER_EMAIL="${GIT_USER_EMAIL}"\
+        --env-file $DOCKER_ENV_FILE \
         -e UPDATE_NAME="${UPDATE_NAME}"\
         -e UPDATE_DESCRIPTION="${UPDATE_DESCRIPTION}"\
         -e UPDATE_VERSION="${UPDATE_VERSION}"\
         -e UPDATE_INFO_URL="${UPDATE_INFO_URL}"\
         -e UPDATE_INFO_TITLE="${UPDATE_INFO_TITLE}"\
-        -e DEBUG=True \
+        -v "$(pwd)/updates:/go" ${DOCKER_IMAGE} \
+        "${TUF_PARAMS}"
+elif [[ $TUF_PARAMS = "create-signature" || $TUF_PARAMS = "sign-signature" || $TUF_PARAMS = "commit-signature" ]]; then
+    echo "=> Create a signature"
+    localread "Please enter the Role (root, targets, snapshot, timestamp):" "" SIGNATURE_ROLE
+    if [[ $TUF_PARAMS = "create-signature" ]]; then
+      localread "Please enter the Name (Person) the key belongs to:" "" SIGNATURE_ROLE_NAME
+    fi
+    docker run \
+        --env-file $DOCKER_ENV_FILE \
+        -e SIGNATURE_ROLE="${SIGNATURE_ROLE}"\
+        -e SIGNATURE_ROLE_NAME="${SIGNATURE_ROLE_NAME}"\
         -v "$(pwd)/updates:/go" ${DOCKER_IMAGE} \
         "${TUF_PARAMS}"
 else
-    docker run -e ACCESS_TOKEN="${ACCESS_TOKEN}"\
-        -e GIT_BRANCH_NAME="${GIT_BRANCH_NAME}"\
-        -e GIT_USER_NAME="${GIT_USER_NAME}"\
-        -e GIT_USER_EMAIL="${GIT_USER_EMAIL}"\
-        -e DEBUG=True \
+    docker run \
+        --env-file $DOCKER_ENV_FILE \
         -v "$(pwd)/updates:/go" ${DOCKER_IMAGE} "${TUF_PARAMS}"
 fi
 
+# Cleanup temp file
+rm $DOCKER_ENV_FILE
 
